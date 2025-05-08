320e40500497fffcfd6c8f1f329191f27a84ecd2
Carlos Cunha <carlosrubim10@gmail.com>
2025-05-08 10:43:48 -0300

    bugfix: --headless

--- scraping.py
+++ scraping.py
@@ -12,7 +12,7 @@ def iniciar_driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--headless") 
+    #options.add_argument("--headless") 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
 
